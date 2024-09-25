a8184538dc6c55b373dd0e6927127f9c3e1d7865
im-afn <arifinppbu@gmail.com>
2024-09-25 22:28:35 +0700

    upload coding portofolio 3

---
+++ assets/js/main.js
@@ -0,0 +1,148 @@
+/*=============== SHOW MENU ===============*/
+const navMenu = document.getElementById("nav-menu");
+const navToggle = document.getElementById("nav-toggle");
+const navClose = document.getElementById("nav-close");
+
+// show menu
+if (navToggle) {
+  navToggle.addEventListener("click", () => {
+    navMenu.classList.add("show-menu");
+  });
+}
+
+//menu hidden
+if (navClose) {
+  navClose.addEventListener("click", () => {
+    navMenu.classList.remove("show-menu");
+  });
+}
+/*=============== REMOVE MENU MOBILE ===============*/
+const navLink = document.querySelectorAll(".nav__link");
+
+const linkAction = () => {
+  const navMenu = document.getElementById("nav-menu");
+  navMenu.classList.remove("show-menu");
+};
+
+navLink.forEach((n) => n.addEventListener("click", linkAction));
+/*=============== SHADOW HEADER ===============*/
+const shadowHeader = () => {
+  const header = document.getElementById("header");
+  window.scrollY >= 50
+    ? header.classList.add("shadow-header")
+    : header.classList.remove("shadow-header");
+};
+
+// Tambahkan event listener untuk memanggil fungsi shadowHeader saat scroll
+window.addEventListener("scroll", shadowHeader);
+
+/*=============== EMAIL JS ===============*/
+const contactForm = document.getElementById("contact-form");
+const contactMessage = document.getElementById("contact-message");
+
+const sendEmail = (e) => {
+  e.preventDefault();
+  // serviceID - templateID - #form - publicKey
+  emailjs
+    .sendForm(
+      "service_8sb5v23",
+      "template_25nnqbm",
+      "#contact-form",
+      "CamMHHiZ-Pp_2VJNK"
+    )
+    .then(
+      () => {
+        // Show sent message
+        contactMessage.textContent = "Message sent successfully ✅";
+        // Remove message after five seconds
+        setTimeout(() => {
+          contactMessage.textContent = "";
+        }, 5000);
+        // Clear input fields
+        contactForm.reset();
+      },
+      () => {
+        // Show error message
+        contactMessage.textContent = " Message not sent (service error) ❌";
+      }
+    );
+};
+contactForm.addEventListener("submit", sendEmail);
+/*=============== SHOW SCROLL UP ===============*/
+const scrollUp = () => {
+  const scrollUp = document.getElementById("scroll-up");
+  this.scrollY >= 350
+    ? scrollUp.classList.add("show-scroll")
+    : scrollUp.classList.remove("show-scroll");
+};
+
+window.addEventListener("scroll", scrollUp);
+/*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
+const sections = document.querySelectorAll("section[id]");
+
+const scrollActive = () => {
+  const scrollDown = window.scrollY;
+
+  sections.forEach((current) => {
+    const sectionHeight = current.offsetHeight;
+    const sectionTop = current.offsetTop - 58;
+    const sectionId = current.getAttribute("id");
+    const sectionClass = document.querySelector(
+      '.nav__menu a[href*="' + sectionId + '"]'
+    );
+
+    if (scrollDown > sectionTop && scrollDown <= sectionTop + sectionHeight) {
+      sectionClass.classList.add("active-link");
+    } else {
+      sectionClass.classList.remove("active-link");
+    }
+  });
+};
+
+window.addEventListener("scroll", scrollActive);
+
+/*=============== DARK LIGHT THEME ===============*/
+const themeButton = document.getElementById("theme-button");
+const darkTheme = "dark-theme";
+const iconTheme = "ri-sun-line";
+
+const selectedTheme = localStorage.getItem("selected-theme");
+const selectedIcon = localStorage.getItem("selected-icon");
+
+const getCurrentTheme = () =>
+  document.body.classList.contains(darkTheme) ? "dark" : "light";
+const getCurrentIcon = () =>
+  themeButton.classList.contains(iconTheme)
+    ? "ri-moon-clear-line"
+    : "ri-sun-line";
+
+if (selectedTheme) {
+  document.body.classList[selectedTheme === "dark" ? "add" : "remove"](
+    darkTheme
+  );
+  themeButton.classList[
+    selectedIcon === "ri-moon-clear-line" ? "add" : "remove"
+  ](iconTheme);
+}
+themeButton.addEventListener("click", () => {
+  document.body.classList.toggle(darkTheme);
+  themeButton.classList.toggle(iconTheme);
+
+  localStorage.setItem("selected-theme", getCurrentTheme());
+  localStorage.setItem("selected-icon", getCurrentIcon());
+});
+
+/*=============== SCROLL REVEAL ANIMATION ===============*/
+const sr = ScrollReveal({
+  origin: "top",
+  distance: "60px",
+  duration: 2500,
+  delay: 400,
+});
+
+sr.reveal(".home__perfil, .contact__mail", { origin: "right" });
+sr.reveal(
+  ".home__name, .home__info, .about__container, .section__title-1, .about__info, contact__social, .contact__data",
+  { origin: "left" }
+);
+sr.reveal(".services__card, .project__card", { interval: 100 });
